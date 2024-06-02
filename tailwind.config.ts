4846dca50d43f13583068288e0ceac24968b98a7
Benson-Craven <bensoncraven@hotmail.co.uk>
2024-06-02 12:24:41 +1000

    Added the initial home screen with the correct font and custom colours added to the tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#F1E8DE',
+        secondary: '#323629',
+        textPrimary: '#595358',
+        textSecondary: '#A4AC96',
+        textThird: '#CFDDA5',
+      },
+      fontFamily: {
+        // customFont: ['"Custom Font"', 'sans-serif'],
+        // Add more custom font families as needed
+        PlayfairDisplay: ['Playfair Display', 'serif'],
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
