ae7b24688d59ddd82fa9871cd42831d7c1c593b3
benson-firstpage <benson@firstpage.com.au>
2025-02-13 09:13:46 +1100

    commented out mask text

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
                     {/* <ScienceSection /> */}
                     <Approach />
                     <Services />
-                    <MaskTextSection />
+                    {/* <MaskTextSection /> */}
                     <SVGPathScienceSection />
 
                     {/* <SVGTest /> */}
