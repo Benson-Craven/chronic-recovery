5335a210337d2cbec55a0f2f5324076ce94d119b
Mark Sellors <msellors@gmail.com>
2025-11-06 10:12:56 +0000

    sitemap for indexing and other bits

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "chronic-recovery",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      cwd: "/opt/chronic-recovery",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
