16ab22a4b2095d5a1aeea387f9ee4420fcdc2731
Mark Sellors <msellors@gmail.com>
2025-11-22 17:25:48 +0000

    fix contact form positioning, make it relative to button.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -262,13 +262,13 @@ const Navbar = ({ className }: NavbarProps) => {
         <AnimatePresence>
           {isActive && (
             <motion.div
-              className="fixed inset-0 z-50 flex items-center justify-center bg-transparent"
+              className="fixed inset-0 z-50 flex items-start justify-end md:justify-center"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
             >
               <motion.div
-                className="absolute top-1/2 left-1/2 z-50 flex w-[80%] max-w-5xl flex-col -translate-x-1/2 -translate-y-1/2 rounded-[25px] border-2 border-black bg-textSecondary shadow-lg"
+                className="absolute top-20 right-20 z-50 flex w-[80%] max-w-5xl flex-col rounded-[25px] border-2 border-black bg-textSecondary shadow-lg md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2"
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
                 exit={{ opacity: 0, scale: 0.9 }}
